0de49dac847ea11036822e1fbe79bbf7fda4d24d
Heze <64218603+Hcmwebs@users.noreply.github.com>
2021-05-18 16:17:20 +1200

    added some automation and manual
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const body = document.body;
 const btn  = document.getElementById("primaryBtn");
 const colorText = document.getElementById("colorText" );
 
-btn.addEventListener('click', () => {
+let timer = 15000;
+
+const changeBg = () => {
 
   const color1 = getRandomRGBColor();
   const color2 = getRandomRGBColor();
@@ -12,8 +14,18 @@ btn.addEventListener('click', () => {
 
   body.style.background = colorName;
   colorText.innerText = colorName;
-})
+
+};
+
+
+//manually
+btn.addEventListener('click', changeBg);
+
+//automatically
+
+window.addEventListener('DOMContentLoaded', () =>{
+
+  setInterval(changeBg, timer);
+});
 
 const getRandomRGBColor = () => Math.floor(Math.random() * 256);
-
-
